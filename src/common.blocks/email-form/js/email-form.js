4061b1ccf5b7deda27cff6f86d0f492119d325d3
imin314 <imin314@gmail.com>
2018-08-26 00:17:38 +0700

    validation check has been added

---
+++ src/common.blocks/email-form/js/email-form.js
@@ -0,0 +1,63 @@
+/*var form = document.querySelector('.email-form');
+var email = document.getElementById('email');
+var bubble = document.querySelector('.email-form__bubble');
+
+
+email.addEventListener("blur", function (event) {
+  if (!email.validity.valid) {
+	bubble.classList.remove("email-form__bubble_good");
+    bubble.classList.add("email-form__bubble_error");
+    bubble.innerHTML = "Error";
+    console.log('ERROR');
+  }
+  else {
+    bubble.classList.remove("email-form__bubble_error");
+	bubble.classList.add("email-form__bubble_good");
+	bubble.innerHTML = "Thanks!";
+	console.log('NO ERROR');
+  }
+}, false);
+
+form.addEventListener("submit", function (event) {
+  if (!email.validity.valid) {
+    event.preventDefault();
+  }
+}, false);*/
+import 'jquery-validation';
+
+$(document).ready(function(){
+	var form = $('#email-form');
+	var inputs = form.find('input[type=text],input[type=email]');
+	
+	form.validate({
+    	errorPlacement: function(error, element) {
+    	}
+    });
+    
+	inputs.each(function () {
+		var input = $(this);
+		input.blur(function() {
+			var bubble = input.next();
+			console.log(bubble);
+			if(!input.valid()) {
+				bubble.removeClass("email-form__bubble_good").addClass("email-form__bubble_error");
+				bubble.text("Error");
+			}
+			else{
+				bubble.removeClass("email-form__bubble_error").addClass("email-form__bubble_good");
+				bubble.text("Thanks!");
+			}
+		});
+	});
+    
+    
+});
+
+/*
+$.fn.checkValidity = function () {
+	
+	/*
+	form.submit(function (event) {
+		
+	});
+};*/
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './common.blocks/button/js/button.js';
 import './common.blocks/percentage-chart/js/percentage-chart.js'
 import './common.blocks/donut-chart/js/donut-chart.js'
 import './common.blocks/slider/js/slider.js'
+import './common.blocks/email-form/js/email-form.js'
