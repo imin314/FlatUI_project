5bba4f5804c12844ebe7a58d677f3d7e849bd974
imin314 <imin314@gmail.com>
2018-10-26 17:21:10 +0700

    chart section updated according to Perfect Pixel

--- src/common.blocks/chart-section/js/chart-section.js
+++ src/common.blocks/chart-section/js/chart-section.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $("#percentage-chart1").updateDonutChart();
-    $("#percentage-chart2").updateDonutChart();
-    $("#percentage-chart3").updateDonutChart();
-    $("#percentage-chart4").updateDonutChart();
+    $("#percentage-chart1").addDonutChart([0], ['#e75735']);
+    $("#percentage-chart2").addDonutChart([38], ['#e75735']);
+    $("#percentage-chart3").addDonutChart([62], ['#e75735']);
+    $("#percentage-chart4").addDonutChart([89], ['#e75735']);
     $("#donut-chart").addDonutChart([12.5,25,30], ['#747474','#e75735','#4eb7a8']);
 });
--- src/common.blocks/donut-chart/js/donut-chart.js
+++ src/common.blocks/donut-chart/js/donut-chart.js
@@ -7,16 +7,21 @@ function $s(elem) {
 // var colors = ['#747474','#e75735','#4eb7a8']
 $.fn.addDonutChart = function (data, colors) {
 	var el = $(this);
-	var $svg = $s("svg").attr({width: '100%', height: '100%', viewBox: '-2 -2 35.830988618379 35.830988618379'});
-	var $circle = $s("circle").attr({cx: 15.91549430918954, cy: 15.91549430918954, r: 15.91549430918954, fill: 'transparent', stroke: '#e5e5e5'});
-	$circle.attr("stroke-width",4);
+	var radius = 15.91549430918954;
+	var backgroundColor = '#e5e5e5';
+	if (data[0] === 0 && data.length === 1) {
+		backgroundColor = 'transparent';
+	}
+	var $svg = $s("svg").attr({width: '100%', height: '100%'});
+	var $circle = $s("circle").attr({cx: radius, cy: radius, r: radius, fill: 'transparent', stroke: backgroundColor});
+	//$circle.attr("stroke-width",4);
 	$svg.append($circle);
 	
 	var offset = 0;
 	var dashoffset0 = 25;
 	for (var i=0; i< data.length; i++){
-		var $segment = $s("circle").attr({cx: 15.91549430918954, cy: 15.91549430918954, r: 15.91549430918954, fill: 'transparent', stroke: colors[i]});
-		$segment.attr("stroke-width",4);
+		var $segment = $s("circle").attr({cx: radius, cy: radius, r: radius, fill: 'transparent', stroke: colors[i]});
+		//$segment.attr("stroke-width",4);
 		var dashoffset = 100 - offset + dashoffset0;
 		$segment.attr("stroke-dasharray", data[i]+' '+(100-data[i]));
 		if (i === 0){
@@ -28,19 +33,14 @@ $.fn.addDonutChart = function (data, colors) {
 		$svg.append($segment);
 		offset += data[i];
 	}
-	
-	/*
-	var $segment2 = $s("circle").attr({cx: 15.91549430918954, cy: 15.91549430918954, r: 15.91549430918954, fill: 'transparent', stroke: '#e75735'});
-	$segment2.attr("stroke-width",4);
-	$segment2.attr("stroke-dasharray", '25 75');
-	$segment2.attr("stroke-dashoffset", 112.5); //100-12.5+25
-	$svg.append($segment2);
-	
-	var $segment3 = $s("circle").attr({cx: 15.91549430918954, cy: 15.91549430918954, r: 15.91549430918954, fill: 'transparent', stroke: '#4eb7a8'});
-	$segment3.attr("stroke-width",4);
-	$segment3.attr("stroke-dasharray", '30 70');
-	$segment3.attr("stroke-dashoffset", 87.5); //100-12.5-25+25
-	$svg.append($segment3);
-	*/
 	el.append($svg);
+	var strokeWidth = parseInt(el.find("circle").css("stroke-width"));
+	var viewBoxTop = 0 - (strokeWidth/2);
+	var viewBoxBottom = radius * 2 + strokeWidth;
+	var viewBoxValue = viewBoxTop + ' '+ viewBoxTop + ' ' + viewBoxBottom + ' ' + viewBoxBottom;
+	//$svg.setAttribute("viewBox",);
+	$svg.attr({viewBox: viewBoxValue});
+	if (el.has(".donut-chart__label")){
+		el.find(".donut-chart__label").text(data[0]);
+	}
 };
