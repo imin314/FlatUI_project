dd8135735e160fa616d09434b066cc49a5b3b2fe
imin314 <imin314@gmail.com>
2018-11-25 14:30:20 +0700

    event card carousel added

---
+++ src/common.blocks/event-carousel/js/event-carousel.js
@@ -0,0 +1,12 @@
+$(window).on("load",function(){
+	$(".event-carousel__cards").mCustomScrollbar({
+		axis:"x",
+		theme:"dark-thin",
+		autoExpandScrollbar:true,
+		advanced:{autoExpandHorizontalScroll:true}
+	});
+
+	$('.event-carousel__card').find('.button').dcalendarpicker({
+		theme: "blue"
+	});
+});
