06cd01ccddf59706f0484dc8e45a07d339d3f73e
imin314 <imin314@gmail.com>
2018-12-04 20:37:16 +0700

    hover effects on touch devices fixed

--- src/common.blocks/button/js/button.js
+++ src/common.blocks/button/js/button.js
@@ -1,7 +1,7 @@
 import './jquery.materialripple.js';
 	$(function(){
 		$('.button').click(function(){
-			var currentColors = $(this).css(["background-color", "color"]);
+			var currentColors = $('.button:active').css(["background-color", "color"]);
 			if (!$(this).hasClass("button_color_reverse")){
 			$(this).css({
 				"background-color": currentColors["background-color"],
@@ -10,5 +10,6 @@ import './jquery.materialripple.js';
 			});
 		}
 		});
+		
 		$('.ripple').materialripple();
 	});
