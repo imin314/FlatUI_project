4ff4ef3e27c8b09c2d9a4d421e8db43f97502a6d
imin314 <imin314@gmail.com>
2019-09-12 09:38:41 +0700

    slider section refactoring

--- src/common.blocks/slider/js/slider.js
+++ src/common.blocks/slider/js/slider.js
@@ -1,37 +1,26 @@
-import './asRange.js'
+import $ from 'jquery';
+import './asRange';
 
 class Slider {
-	constructor(domElement) {
-		this.domElement = $(domElement);
-		this.namespace = this.domElement[0].className.split(" ")[0];
-		this.initialize();
-	}
+  constructor(domElement) {
+    this.$domElement = $(domElement);
+    this._initialize();
+  }
 
-	initialize() {
-		$(document).ready(() => {
-			$(`.${this.namespace}_type_tooltip`).asRange({
-				tip: { 
-					active: 'onMove',
-				},
-				step: 0.1
-			}); 
-				
-			$(`.${this.namespace}_type_scale`).asRange({
-				tip: false,
-				scale: {
-					scale: {
-						valuesNumber: 4,
-						gap: 1,
-						grid: 5
-					}
-				},
-				step: 0.1
-			});
-		});
-	}
+  _initialize() {
+    $(document).ready(() => {
+      const $slider = this.$domElement;
+      const settings = {
+        tip: false,
+        step: 0.1,
+      };
+      if ($slider.hasClass('js-slider_type_tooltip')) {
+        settings.tip = { active: 'onMove' };
+      }
 
+      $slider.asRange(settings);
+    });
+  }
 }
 
-$(".slider").each(function() {
-	new Slider(this);
-});
+$('.js-slider').each((i, element) => new Slider(element));
