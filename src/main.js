cf3a287ed035c68e55ad2004ec1e993d53fca21c
imin314 <imin314@gmail.com>
2019-09-30 10:40:54 +0700

    pug refactoring + user-card renamed to profile-card #1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+//import './assets/favicons/favicons';
+
 function importAll(r) {
   r.keys().forEach(r);
 }
 
 importAll(require.context('./', true, /\.(styl)$/));
-importAll(require.context('./common.blocks/', true, /\.(js)$/));
+importAll(require.context('./assets/favicons', false, /\.(svg|png|ico|xml|json)$/));
+importAll(require.context('./common.blocks', true, /\.(js)$/));
+importAll(require.context('./pages', true, /\.(js)$/));
