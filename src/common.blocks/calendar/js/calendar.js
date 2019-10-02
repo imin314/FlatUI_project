1d1e23090c55abe140606cdce2d343d7cbeabdc2
imin314 <imin314@gmail.com>
2019-10-03 02:44:22 +0700

    calendar block - continue moving to jQuery UI #6

--- src/common.blocks/calendar/js/calendar.js
+++ src/common.blocks/calendar/js/calendar.js
@@ -7,28 +7,67 @@ class Calendar {
 
   _initialize(domElement) {
     $(document).ready(() => {
-      this._findElements(domElement);
+      this.$calendar = $(domElement);
+      this._prependDayContainer();
+      
       const settings = this._generateCalendarSettings();
       this.$calendar.datepicker(settings);
+      
+      this._updateDayText(this.$calendar.datepicker('getDate'));
     });
-  }
-
-  _findElements(domElement) {
-    const $calendar = $(domElement);
-    this.$calendar = $calendar;
   }
 
   _generateCalendarSettings() {
     const settings = {
-      showButtonPanel: true,
-      firstDate: 1,
+      firstDay: 1,
       prevText: '',
       nextText: '',
       showOtherMonths: true,
       dayNamesMin: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ],
+      onChangeMonthYear: (y, m) => this._handleMonthChange(y, m),
+      onSelect: (date) => this._updateDayText(date),
     };
 
     return settings;
+  }
+
+  _prependDayContainer() {
+    const $dayContainer = $('<div></div>', { class: 'calendar__day-container' });
+    this.$dayText = $('<span></span>', {
+      class: 'calendar__day-text',
+      type: 'text',
+      disabled: 'disabled',
+    });
+    $dayContainer.prepend(this.$dayText);
+    
+    this.$calendar.prepend($dayContainer);
+
+    return this;
+  }
+
+  _appendButtonPane() {
+    const $buttonPane = $('<div></div>', { class: 'calendar__button-pane' });
+    this.$button = $('<button></button>', {
+      class: 'calendar__today-button',
+      type: 'button',
+    });
+    $buttonPane.append(this.$button);
+    
+  }
+
+  _updateDayText(value) {
+    const date = new Date(value);
+    this.$dayText.text(date.getDate());
+
+    return this;
+  }
+
+  _handleMonthChange(year, month) {
+    const newDate = `${month}/1/${year}`;
+    this.$calendar.datepicker('setDate', newDate);
+    this._updateDayText(newDate);
+
+    return this;
   }
 }
 
